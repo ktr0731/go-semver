52592e70e6670bdb30bb3c272d0a41ac889055f6
ktr0731 <aizu.s1230022@gmail.com>
2018-04-17 09:04:33 +0900

    add Bump test

--- semver_test.go
+++ semver_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSemVer(t *testing.T) {
@@ -27,5 +28,32 @@ func TestSemVer(t *testing.T) {
 			assert.NoError(t, v.Error())
 			assert.Equal(t, c.expected, v)
 		}
+	}
+}
+
+func TestSemVer_Bump(t *testing.T) {
+	cases := []struct {
+		version string
+		bumped  string
+		major   bool
+		minor   bool
+		patch   bool
+	}{
+		{version: "0.2.3", bumped: "1.0.0", major: true},
+		{version: "0.1.9", bumped: "0.2.0", minor: true},
+		{version: "0.1.9", bumped: "0.1.10", patch: true},
+	}
+	for _, c := range cases {
+		v := New(c.version)
+		require.NoError(t, v.Error())
+		switch {
+		case c.major:
+			v.Bump(VersionTypeMajor)
+		case c.minor:
+			v.Bump(VersionTypeMinor)
+		case c.patch:
+			v.Bump(VersionTypePatch)
+		}
+		assert.Equal(t, c.bumped, v.String())
 	}
 }
